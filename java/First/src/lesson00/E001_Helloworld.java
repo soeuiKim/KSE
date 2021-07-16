16f5b13f74d034d2c9f39c187600c76456de8c13
soeuiKim <thdml743@naver.com>
2021-07-16 09:40:40 +0900

    Initial commit

---
+++ java/First/src/lesson00/E001_Helloworld.java
@@ -0,0 +1,10 @@
+package lesson00;
+
+public class E001_Helloworld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hi, Java oO0lIiIlL1");
+	}
+
+}
---
+++ python/pycharmprojects/prj001/first.py
@@ -0,0 +1,3 @@
+birthyear =  int(input("태어난 해는?"))
+age = 2021 - birthyear
+print("당신의 나이는" + str(age)+ "살 입니다.")
